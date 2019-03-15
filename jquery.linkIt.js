42b257779705c524a77105a08fb8c2b33e1ce933
joelbolt35 <joelemora@yahoo.com>
2019-03-15 03:54:12 -0400

    commit #2

---
+++ jquery.linkIt.js
@@ -0,0 +1,32 @@
+/*
+name: LinkIt
+author: Joel Mora
+version: 0.1.0
+license: MIT
+*/
+
+(function($) {
+    $.fn.linkIt = function(options) {
+        //Default Settings
+        var settings = $.extend({
+            href: null,
+            text: null,
+            target: '_blank'
+        }, options);
+
+        //Validation
+        if(settings.href == null) {
+            console.log('You need an href option for LinkIt to work');
+            return this;
+        }
+
+        return this.each(function() {
+            var object = $(this);
+
+            if (settings.text == null) {
+                settings.text = object.text();
+            }
+
+        });
+    }
+}(jQuery));
